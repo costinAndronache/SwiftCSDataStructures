4bcd5697a7a98fe4ae638db6fc2e9e84e80b5d50
costinAndronache <andronache.costin@gmail.com>
2017-11-01 23:35:42 +0200

    Implemented "unionFind" or "Disjoint sets forest"
    Plus some utility methods on graphs

--- SwiftCSDataStructures.swift
+++ SwiftCSDataStructures.swift
@@ -1,3 +1,6 @@
+//  Write some awesome Swift code, or import libraries like "Foundation",
+//  "Dispatch", or "Glibc"
+
 public struct Queue<T> {
     
     private var queue: [T] = []
@@ -187,13 +190,108 @@ public struct Heap<T: Comparable, U: Hashable> {
 	}
 }
 
+
+
+public struct DisjointSetsForest<T: Hashable> {
+	private var parent: [T: T] = [:]
+	private var level: [T: Int] = [:]
+	
+	
+	public mutating func makeSetFor(_ value: T) {
+		parent[value] = value
+		level[value] = 1
+	}
+	
+	public mutating func inSameSet(_ x: T, _ y: T) -> Bool {
+		guard let rx = representativeFor(x),
+			  let ry = representativeFor(y),
+			  rx == ry else {
+				  return false
+			  }
+		return true
+	}
+	
+	public mutating func uniteInSameSet(_ x: T, _ y: T){
+		guard let x = representativeFor(x), 
+			  let y = representativeFor(y) else {
+			return
+		}
+		
+		let xLevel = levelFor(x)
+		let yLevel = levelFor(y)
+		
+		if xLevel > yLevel {
+			parent[y] = x 
+		} else {
+			parent[x] = y
+		}
+		
+		if xLevel == yLevel {
+			level[y] = yLevel + 1
+		}
+	}
+	
+	private mutating func levelFor(_ value: T) -> Int {
+		return self.level[value] ?? 0
+	}
+	
+	private mutating func representativeFor(_ value: T) -> T? {
+		var currentValue = value
+		while let ancestor = parent[currentValue], 
+				  ancestor != currentValue {
+			currentValue = ancestor
+		}
+		
+		
+			var x = value
+			while let ancestor = parent[x], 
+				  ancestor != currentValue {
+				  parent[x] = currentValue
+				  x = ancestor
+			}
+		
+		
+		return currentValue
+	}
+}
+
+public struct Edge: Hashable {
+	public let from: Int
+	public let to: Int
+	
+	public var hashValue: Int {
+		return "\(from)\(to)".hashValue
+	} 
+}
+
+public func ==(lhs: Edge, rhs: Edge) -> Bool {
+	return lhs.from == rhs.from && 
+		   lhs.to == rhs.to
+}
+
+public struct WeightedEdge: Hashable {
+	public let edge: Edge
+	public let cost: Double
+	
+	public var hashValue: Int {
+		return edge.hashValue
+	}
+}
+
+public func ==(lhs: WeightedEdge, rhs: WeightedEdge) -> Bool {
+	return lhs.edge == rhs.edge
+}
+
 public protocol GraphProtocol {
 	func neighboursFor(node: Int) -> [Int]
+	func degreeFor(node: Int) -> Int?
 	func allNodes() -> [Int]
+	func allEdges() -> [Edge]
 }
 
 public protocol WeightedGraphProtocol: GraphProtocol {
 	func costForEdge(from: Int, to: Int) -> Double?
+	func allEdgesWeighted() -> [WeightedEdge]
 }
 
 
@@ -212,8 +310,36 @@ public struct WeightedGraph: WeightedGraphProtocol {
 		case undirected
 	}
 	
+	public func degreeFor(node: Int) -> Int? {
+		return self.adjancencyListPerNode[node]?.count
+	}
+	
 	public func allNodes() -> [Int] {
 		return Array<Int>(self.adjancencyListPerNode.keys)
+	}
+	
+	public func allEdges() -> [Edge] {
+		var edgesSet: Set<Edge> = Set<Edge>()
+		
+		self.allNodes().forEach { node in 
+			let neighbours = self.neighboursFor(node: node)
+			neighbours.forEach {
+				let edge = Edge(from: node, to: $0)
+				if self.type == .directed {
+					edgesSet.insert(edge)
+				} else if !edgesSet.contains(Edge(from: $0, to: node)) {
+					edgesSet.insert(edge)
+				}
+			}
+		}
+		
+		return Array<Edge>(edgesSet)
+	}
+	
+	public func allEdgesWeighted() -> [WeightedEdge]{
+		return self.allEdges().map({
+			return WeightedEdge(edge: $0, cost: self.costForEdge($0)!)
+		})
 	}
 	
     public mutating func addNodeIfNotExisting(_ node: Int){
@@ -254,7 +380,9 @@ public struct WeightedGraph: WeightedGraphProtocol {
 		return self.costMapPerNode[from]?[to]
 	}
 	
-    
+    public func costForEdge(_ edge: Edge) -> Double? {
+		return self.costForEdge(from: edge.from, to: edge.to)
+	}
 
 }
 
@@ -373,4 +501,100 @@ public struct ShortestPath {
 		
 		return .result(distancePerNode: distanceForNode)
 	}
+}
+
+
+public struct Connectivity {
+	public static func isConnected(graph: GraphProtocol) -> Bool {
+		var visited: Set<Int> = Set<Int>()
+		let nodes = graph.allNodes()
+		guard let firstNode = nodes.first else {
+			return false
+		}
+		
+		var queue = Queue<Int>()
+		queue.enqueue(firstNode)
+		while let node = queue.dequeue() {
+			let neighbours = graph.neighboursFor(node: node)
+			for u in neighbours {
+				if !visited.contains(u) {
+					visited.insert(u)
+					queue.enqueue(u)
+				}
+			}
+		}
+		
+		for v in nodes {
+			if !visited.contains(v) {
+				return false
+			}
+		}
+		
+		return true
+	}
+}
+
+
+public struct Cycles {
+	public enum EulerianCycleResult {
+		case graphNotConnected
+		case oddDegreeForNode(node: Int)
+		case result(nodesOrder: [Int])
+	}
+	
+	public static func eulerianCycleIn(graph: GraphProtocol) -> EulerianCycleResult {
+		guard Connectivity.isConnected(graph: graph) else {
+			return .graphNotConnected
+		}
+		
+		let nodes = graph.allNodes()
+		let firstOddDegreeNode = nodes.first(where: {(graph.degreeFor(node: $0) ?? 0) % 2 == 1})
+		guard firstOddDegreeNode == nil else {
+			return .oddDegreeForNode(node: firstOddDegreeNode!)
+		}
+		
+		
+		//will continue later
+		return EulerianCycleResult.result(nodesOrder: [])
+	}
+}
+
+
+public struct Math {
+	public static func gcd(_ x: Int, _ y: Int) -> Int {
+		var a = max(x, y)
+		var b = min(x, y)
+
+		var r = a % b
+		while(r != 0 ) {
+			a = b
+			b = r  
+			r = a % b
+		}
+	
+		return b
+	}
+
+	public static func lcm(_ a: Int, _ b: Int) -> Int {
+		return (a*b)/(Math.gcd(a,b))
+	}
+	
+	public struct Fraction: Hashable {
+		public let numerator: Int
+		public let denominator: Int 
+		public init?(numerator: Int, denominator: Int){
+			let d = Math.gcd(numerator, denominator)
+			self.numerator = numerator / d
+			self.denominator = denominator / d
+		}
+		
+		public var hashValue: Int {
+			return numerator ^ denominator
+		}
+	}
+}
+
+public func ==(lhs: Math.Fraction, rhs: Math.Fraction) -> Bool {
+	return lhs.numerator == rhs.numerator && 
+		   lhs.denominator == rhs.denominator
 }
